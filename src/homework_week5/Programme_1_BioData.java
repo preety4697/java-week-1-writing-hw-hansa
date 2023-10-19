44a2f34bcece775b4a2394b476a07e091df4c2ea
preety4697 <147104856+preety4697@users.noreply.github.com>
2023-10-19 21:54:11 +0100

    homework

---
+++ src/homework_week5/Programme_1_BioData.java
@@ -0,0 +1,23 @@
+package homework_week5;
+
+/**
+ * Write a programme that prints your BIO DATA
+ */
+public class Programme_1_BioData {
+    public static void main(String[] args) {
+        System.out.println("             BIO DATA           ");
+        System.out.println("Name             : Jane Smith");
+        System.out.println("Mobile           : 07912 345678");
+        System.out.println("Email            : janesmith@gmail.com");
+        System.out.println("Gender           : Female");
+        System.out.println("Date Of Birth    : 01.05.2000");
+        System.out.println("Marital Status   : single");
+        System.out.println("Language known   : English,Spanish and German");
+        System.out.println("Qualification    : Software tester");
+        System.out.println("Experience       : Agile tester of 5 years experience");
+        System.out.println("Address          : 123 abc street London AB1 2CD");
+        System.out.println("Place            : London");
+        System.out.println("Date             : 19.10.2023");
+        System.out.println("Signature        : JSmith");
+    }
+}
---
+++ src/homework_week5/Programme_8_DivisionOfTwoNumbers.java
@@ -0,0 +1,19 @@
+package homework_week5;
+
+/**
+ * 8. Write a java programme to divide two numbers and print on the screen.
+ * Test data : 50/3
+ * Expected output : 16
+ */
+public class Programme_8_DivisionOfTwoNumbers {
+    public static void main(String[] args) {
+        // The two integer are value1 and value2
+        int value1 = 50;
+        int value2 = 3;
+        int division = value1 / value2;
+        //concatenation
+        System.out.println("The "+ value1  +  " is divided by " + "the answer is " + division);
+
+    }
+
+}
